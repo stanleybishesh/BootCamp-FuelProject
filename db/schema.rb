06d11d341ffbd3048c70099d361de8c9048d3b63
stanleybishesh <unikbisheshshrestha12@gmail.com>
2024-08-30 10:47:37 +0000

    Venue model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_30_103417) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_30_104618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,5 +55,11 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_30_103417) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["tenant_id"], name: "index_users_on_tenant_id"
+  end
+
+  create_table "venues", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
