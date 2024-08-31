14ad4f3e41b8d5b230261a80d736c242b6c85a96
stanleybishesh <unikbisheshshrestha12@gmail.com>
2024-08-31 06:20:53 +0000

    OrderGroup model added with some associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_30_104618) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_31_054405) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,16 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_30_104618) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_couriers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_couriers_on_reset_password_token", unique: true
+  end
+
+  create_table "order_groups", force: :cascade do |t|
+    t.integer "tenant_id"
+    t.date "start_on"
+    t.datetime "completed_on"
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tenant_id"], name: "index_order_groups_on_tenant_id"
   end
 
   create_table "tenants", force: :cascade do |t|
