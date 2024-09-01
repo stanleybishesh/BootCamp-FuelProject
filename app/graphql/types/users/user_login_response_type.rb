62d2864e0ed5b708947406029753bc54307487e5
bipin thapa <mine.mememimi@gmail.com>
2024-09-01 18:35:30 +0000

    changed git lint issues

--- app/graphql/types/users/user_login_response_type.rb
+++ app/graphql/types/users/user_login_response_type.rb
@@ -2,7 +2,7 @@ module Types
   module Users
     class UserLoginResponseType < Types::BaseObject
       field :user, Types::Users::UserType, null: true
-      field :errors, [String], null: false
+      field :errors, [ String ], null: false
     end
   end
 end
