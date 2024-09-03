3d5f9e0a06f97a87f0672a85f718d1c3b47c80f0
stanleybishesh <unikbisheshshrestha12@gmail.com>
2024-09-03 06:16:57 +0000

    JWT changes

--- app/graphql/mutations/users/user_sign_up.rb
+++ app/graphql/mutations/users/user_sign_up.rb
@@ -6,7 +6,7 @@ module Mutations
       field :user, Types::Users::UserType, null: true
       field :errors, [ String ], null: false
 
-      def resolve (user_data:)
+      def resolve(user_data:)
         tenant = Tenant.find_by(id: user_data.tenant_id)
         raise GraphQL::ExecutionError, "Tenant does not exist !" if tenant.nil?
 
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,5 +3,6 @@
 module Types
   class MutationType < Types::BaseObject
     field :user_signup, mutation: Mutations::Users::UserSignUp
+    field :login, mutation: Mutations::Users::Login
   end
 end
