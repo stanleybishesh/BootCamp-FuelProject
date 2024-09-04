a1c6b8c54fa7e34eaade87d8c7649782a2d85f23
stanleybishesh <unikbisheshshrestha12@gmail.com>
2024-09-04 19:07:51 +0000

    ActsAsTenant recorrected

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -11,8 +11,9 @@ class GraphqlController < ApplicationController
     query = params[:query]
     operation_name = params[:operationName]
     context = {
-      current_user: current_user_from_token
-      }
+      current_user: current_user_from_token,
+      current_tenant: ActsAsTenant.current_tenant
+    }
     result = FuelPandaSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   rescue StandardError => e
