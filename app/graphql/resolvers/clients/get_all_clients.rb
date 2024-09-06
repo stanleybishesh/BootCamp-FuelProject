2a7febf9430e8a60701e8ac9dc9ec232c1d338ca
stanleybishesh <unikbisheshshrestha12@gmail.com>
2024-09-06 05:27:19 +0000

    Client resolver corrected

--- app/graphql/resolvers/clients/get_all_clients.rb
+++ app/graphql/resolvers/clients/get_all_clients.rb
@@ -5,7 +5,9 @@ class Resolvers::Clients::GetAllClients < Resolvers::BaseResolver
     user = current_user
     if user
       ActsAsTenant.with_tenant(user.tenant) do
-        Client.all
+        memberships = Membership.all
+        clients = Client.where(id: memberships.select(:client_id))
+        clients
       end
     else
       raise GraphQL::ExecutionError, "User not logged in"
