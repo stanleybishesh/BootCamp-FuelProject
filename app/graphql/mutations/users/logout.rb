97cdee0d20ad2e09a3dcd7db61286c4c9204db77
bipin thapa <mine.mememimi@gmail.com>
2024-09-09 08:03:42 +0000

    removed repeated codes from logout mutations

--- app/graphql/mutations/users/logout.rb
+++ app/graphql/mutations/users/logout.rb
@@ -1,24 +1,15 @@
 module Mutations
   module Users
     class Logout < BaseMutation
-      include TokenHelper
-
       field :success, Boolean, null: true
       field :message, String, null: true
 
       def resolve
-        token = context[:headers]["Authorization"].to_s.split(" ").last
-        return { success: false, message: "User not logged in" } unless token
-
-        decoded_token = JWT.decode(token, "secret", true, algorithm: "HS256")[0]
-        return { success: false, message: "User not logged in" } unless decoded_token["type"] == "user" && decoded_token["user_id"]
-
-        user = User.find_by(id: decoded_token["user_id"])
-
-        if user && user.jti == decoded_token["jti"]
+        user = current_user
+        if user
           ActsAsTenant.with_tenant(user.tenant) do
             new_jti = SecureRandom.uuid
-            user.update(jti: new_jti) # Invalidate the JTI
+            user.update(jti: new_jti)
             { success: true, message: "Logged Out Successfully!" }
           end
         else
--- app/helpers/token_helper.rb
+++ app/helpers/token_helper.rb
@@ -6,6 +6,8 @@ module TokenHelper
     decoded_token = JWT.decode(token, "secret", true, algorithm: "HS256")[0]
     entity_type = decoded_token["type"]
 
+    decoded_token["type"] == "user" && decoded_token["user_id"]
+
     if entity_type == "user" && decoded_token["user_id"]
       user = User.find_by(id: decoded_token["user_id"])
       return user if user&.jti == decoded_token["jti"]
