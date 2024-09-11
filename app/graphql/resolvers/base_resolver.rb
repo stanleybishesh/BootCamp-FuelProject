4d67b300e2e65421bd615d7e7da5b32d39780f47
stanleybishesh <unikbisheshshrestha12@gmail.com>
2024-09-11 07:52:47 +0000

    Uncommented current_user base resolver

--- app/graphql/resolvers/base_resolver.rb
+++ app/graphql/resolvers/base_resolver.rb
@@ -1,7 +1,11 @@
 module Resolvers
   class BaseResolver < GraphQL::Schema::Resolver
-    # def current_user
-    #   context[:current_user]
-    # end
+    def current_user
+      context[:current_user]
+    end
+
+    def current_courier
+      context[:current_courier]
+    end
   end
 end
