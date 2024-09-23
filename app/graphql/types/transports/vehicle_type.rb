684dbd484ce4c034c3f69f45ea962494f73a2257
stanleybishesh <unikbisheshshrestha12@gmail.com>
2024-09-23 11:39:16 +0000

    Vehicle Type corrected

--- app/graphql/types/transports/vehicle_type.rb
+++ app/graphql/types/transports/vehicle_type.rb
@@ -1,10 +1,10 @@
 module Types
   module Transports
     class VehicleType < Types::BaseEnum
-      value "Tank", value: "tank"
-      value "TankWagon", value: "tank_wagon"
-      value "Truck", value: "truck"
-      value "SemiTruck", value: "semi_truck"
+      value "tank", value: "tank"
+      value "tank_wagon", value: "tank_wagon"
+      value "truck", value: "truck"
+      value "semi_truck", value: "semi_truck"
     end
   end
 end
