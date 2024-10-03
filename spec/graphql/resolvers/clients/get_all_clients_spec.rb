55aea7a68b65b77570de82958cd5468b2f0571c8
stanleybishesh <unikbisheshshrestha12@gmail.com>
2024-10-03 18:56:38 +0000

    little correction in get_all_clients_spec.rb

--- spec/graphql/resolvers/clients/get_all_clients_spec.rb
+++ spec/graphql/resolvers/clients/get_all_clients_spec.rb
@@ -12,43 +12,42 @@ module Resolvers
         end
 
         context 'when user is logged in' do
-        it 'retrieves all clients' do
-          ActsAsTenant.with_tenant(tenant) do
-            membership1 = create(:membership)
-            membership2 = create(:membership)
-            client1 = membership1.client
-            client2 = membership2.client
+          it 'retrieves all clients' do
+            ActsAsTenant.with_tenant(tenant) do
+              membership1 = create(:membership)
+              membership2 = create(:membership)
+              client1 = membership1.client
+              client2 = membership2.client
 
-            result = FuelPandaSchema.execute(
-              query,
-              context: { current_user: user },
-              variables: {}
-            )
-
-            data = result['data']['getAllClients']
-
-            expect(data).to be_an(Array)
-            expect(data.size).to eq(2)
-            expect(data).to include(
-              include(
-                'id' => client1.id.to_s,
-                'name' => client1.name,
-                'email' => client1.email,
-                'address' => client1.address,
-                'phone' => client1.phone
-              ),
-              include(
-                'id' => client2.id.to_s,
-                'name' => client2.name,
-                'email' => client2.email,
-                'address' => client2.address,
-                'phone' => client2.phone
+              result = FuelPandaSchema.execute(
+                query,
+                context: { current_user: user },
+                variables: {}
               )
-            )
+
+              data = result['data']['getAllClients']
+
+              expect(data).to be_an(Array)
+              expect(data.size).to eq(2)
+              expect(data).to include(
+                include(
+                  'id' => client1.id.to_s,
+                  'name' => client1.name,
+                  'email' => client1.email,
+                  'address' => client1.address,
+                  'phone' => client1.phone
+                ),
+                include(
+                  'id' => client2.id.to_s,
+                  'name' => client2.name,
+                  'email' => client2.email,
+                  'address' => client2.address,
+                  'phone' => client2.phone
+                )
+              )
+            end
           end
         end
-      end
-
 
         context 'when user is not logged in' do
           before do
